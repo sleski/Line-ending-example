1a53283c159abf5fb59c3851fff9313361a5ada4
Slawomir Leski <slawomir.leski@gmail.com>
2017-03-16 16:14:15 +0100

    Added finaly test ready to use on windows/unix

--- src/test/java/it/tostao/SimpleFileGeneratorTest.java
+++ src/test/java/it/tostao/SimpleFileGeneratorTest.java
@@ -20,8 +20,10 @@ public class SimpleFileGeneratorTest {
 
 		Path resourceDirectory = Paths.get("src/test/resources");
 		File tempFile = new File(resourceDirectory.toFile(), "test.txt");
-		System.out.println("Current line separator is = " + System.lineSeparator());
-		System.setProperty("line.separator", "\r\n");
+		//UNIX
+		//System.setProperty("line.separator", "\n");
+		//WINDOWS
+		//System.setProperty("line.separator", "\r\n");
 		final String sampleText = "This is line nb: ";
 		final List<String> toSave = new ArrayList<String>();
 		for (int counter = 0; counter < 4; counter++) {
